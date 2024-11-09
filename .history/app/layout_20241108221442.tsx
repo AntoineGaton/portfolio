407e91a9cd5038e96acf7d09bcc270c52b45d999
Antoine Gaton <antoinegaton@gmail.com>
2024-11-08 22:15:11 -0500

    commits

---
+++ .history/app/layout_20241108221442.tsx
@@ -0,0 +1,35 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Cursor } from '@/components/cursor';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Antoine Gaton\'s Portfolio',
+  description: 'A Windows 11-style portfolio website created by Software Engineer Antoine Gaton using Next.js, Shadcn, Tailwind CSS, and TypeScript.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={inter.className} suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Cursor />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
